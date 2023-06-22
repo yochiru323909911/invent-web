9bde2cc08acc38dff23f3b40ba5c3eb87af4fcff
liatam <liatam@edu.hac.ac.il>
2023-06-22 22:53:09 +0300

    admin page work

--- src/main/java/hac/controllers/AdminController.java
+++ src/main/java/hac/controllers/AdminController.java
@@ -5,6 +5,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
 
 /*
 לשאול איזה קבצים בידיוק צריך לבוטסראפ
@@ -12,13 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
  */
 @Controller
-
 public class AdminController {
 
     @GetMapping("/admin")
     public String showAdmin() {
 
         return "admin";
+    }
+
+    @GetMapping("/admin/search")
+    public String handleSearch(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        // handle your search logic here...
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        return "results";
     }
 
 }
